d01d40bd371971c837383d334ded2f301174245b
netonblpb <skullbog@gmail.com>
2022-02-03 14:17:24 +0300

    Task6 Complete

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Task6: Выяснить является ли число чётным
+
+int number = 16;
+
+if (number % 2 == 0)
+Console.WriteLine("Число " + number + " четное");
+else
+Console.WriteLine("Число " + number + " нечетное");
